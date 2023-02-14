10fdf0ad076de4f7331efabcf1e7e3f68ea978f0
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-02-15 10:28:21 +1100

    Create first middleware.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+
+app.use(morgan("tiny"));
+app.use((req, res, next) => {
+  console.log("From middleware with love!!!");
+  return next();
+  console.log("Maybe love after calling next() "); // use return before to make sure nothing will running after next()
+});
+app.use((req, res, next) => {
+  console.log("From second middleware with love!!!");
+  return next();
+});
 
 app.get("/", (req, res) => {
   res.send("HOME PAGE");
